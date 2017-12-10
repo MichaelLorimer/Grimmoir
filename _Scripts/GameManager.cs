7942584221924a964f1e35069a64a07633bc1661
MichaelLorimer <michael.lorimer@hotmail.co.uk>
2017-12-10 12:15:08 +0000

    Add files via upload

---
+++ _Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+
+
+	private PlayerControler PlayerChar;
+	private int CurrentHealth;
+	public Text Living;
+	public Text Health;
+	public Text Standing;
+	public Text info;
+
+	private bool GameOver = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		PlayerChar = GetComponent<PlayerControler> ();
+		//Find Current Instance of the player script
+		//PlayerChar = GetComponent<PlayerControler>();
+		//PlayerChar = FindObjectOfType(typeof(PlayerControler));
+		//CurrentHealth = PlayerChar
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		UpdateText ();	
+	}
+
+	void UpdateText()
+	{
+		if (!GameOver) 
+		{
+			Living.text = "Living: yes";
+		}
+	
+		Health.text = "HP: " + PlayerControler.PlayerHealth;
+		Standing.text = "Current Block: " + PlayerControler.block; // Layer Colision Debug code
+	
+		// Kevin debug code
+		//Pos.text
+		info.text = "Kevin: Pos: " + PlayerControler.Position + ", JumpVal: " + PlayerControler.JumpVal + ", Grounded: " + PlayerControler.isGrounded;
+	}
+}
---
+++ _Scripts/PlayerControler.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//--------------------------------------------
+// -------- NEEDS ----------------------------
+//============================================
+
+//Movement
+	//- WD, Space = jump
+	//- Graviity
+
+//Player
+	//Health
+	//Power
+	//Spells
+
+public class PlayerControler : MonoBehaviour 
+{
+	//Player Dater
+	static public int PlayerHealth; // Defined at 50 in the Editor
+	public Rigidbody2D Player;		// Holds the Player(Kevin) RigidBody
+
+	// -- Movement Data
+	public float Speed;		//Movement speed: Set to 5f in the editor
+	public int Gravity = 1; //Gravioty Value ---- Not Used ----
+
+	// - Jump
+	public Transform GroundCheck; //Holds the GroundCheckTransform Object attached to the Player (defined in editor)
+	public LayerMask GroundLayer; //Holds the layer to check if grounded
+
+	private bool Jumped = false; 	//Bool to determine if the Player has jumped (to disable infinite jump)
+	static public bool isGrounded = true; //Bool to determine if the Player is grounded
+	static public float JumpVal;			//Float to store the jump force
+
+
+	//Debug info 
+	static public int block;
+	static public Vector2 Position;
+	// JumpVal 
+	//isGrounded
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		PlayerHealth = 50; // Working Value
+		Player = GetComponent<Rigidbody2D> (); //Find and Cache component
+		Speed = 5;	//Working Value
+	}
+
+	void FixedUpdate()
+	{
+		//check grounded
+		Position = Player.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		isGrounded = Physics2D.OverlapCircle (GroundCheck.position, 0.01f, GroundLayer);
+		Vector2 Grav = Player.velocity;
+		Grav.y = -Gravity;
+		Player.velocity = Grav;
+		//Movement 
+		//float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		//float moveVertical = Input.GetAxisRaw ("Vertical");
+	
+		//Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		//Player.AddForce (movement * Speed);
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			Vector2 Pos = Player.transform.position;
+			Pos.x += 10 * Time.deltaTime;
+			Player.transform.position = Pos;
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			Vector2 Pos = Player.transform.position;
+			Pos.x -= 10 * Time.deltaTime;
+			Player.transform.position = Pos;
+		}
+		//Jump
+
+		/*if (Input.GetKey (KeyCode.W)) 
+		{
+			Player.AddForce (new Vector2 (0, JumpVal));	
+			Jumped = true;
+		}*/
+	}
+
+	void OnCollisionStay2D(Collision2D coll)
+	{
+		block = coll.gameObject.layer;
+		if (coll.gameObject.layer == 8 && (Input.GetKey (KeyCode.W))) 
+		{
+			Player.AddForce(new Vector2 (0, JumpVal));
+		}
+	}
+}
